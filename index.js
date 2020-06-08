0d2d753b1ac100efd4072e1ce3d2d2308836989e
sara altaee <sara.korea96@gmail.com>
2020-06-08 01:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// Add your functions here
-let source=[1,2,3,4,5];
-function getNegtive(source){
-let negative =source.map(x=> x *-1)
-  return negative;
+function map(array, callback) {
+  let newArray = [];
+  array.forEach(item => {
+    newArr.push(callback(item))
+  })
+  return newArr
+}
+
+function reduce(arr, callbackFn, startingPoint) {
+  let newValue = startingPoint ? startingPoint : arr[0]
+  for (let i = startingPoint ? 0 : 1; i < arr.length; i++) {
+    newValue = callbackFn(arr[i], newValue)
+  }
+  return newValue
 }
