a4f2785293124b685d87ccd0268b3bbd2f92e7bf
sara altaee <sara.korea96@gmail.com>
2020-06-08 01:23:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(array, callback) {
+  let newArray = [];
+  array.forEach(item => {
+    newArray.push(callback(item))
+  })
+  return newArray;
+}
+
+function reduce(array, callback, startingPoint) {
+  let newArray2 = startingPoint ? startingPoint : array[0]
+  for (let i = startingPoint ? 0 : 1; i < array.length; i++) {
+    newArray2 = callback(array[i], newArray2)
+  }
+  return newArray2
+}
