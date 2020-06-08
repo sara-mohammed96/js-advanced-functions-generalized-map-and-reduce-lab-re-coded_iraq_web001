e0453100785e54bea4ea40d6960bf7feaecc77d2
sara altaee <sara.korea96@gmail.com>
2020-06-08 01:17:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ function map(array, callback) {
 }
 
 function reduce(array, callback, startingPoint) {
-  let newValue = startingPoint ? startingPoint : arr[0]
-  for (let i = startingPoint ? 0 : 1; i < arr.length; i++) {
-    newValue = callback(arr[i], newValue)
+  let newValue = [];
+    array.forEach(item =>{
+       startingPoint? false:true;
+       newArray2=callback(item)
+      
+    }
+    })
+   
   }
   return newValue
 }
